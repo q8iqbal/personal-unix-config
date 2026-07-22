504e2dc915c848c6ddf478e0a965dc7c9ac28800
Iqbal Nur Imansyah <iqbal.imansyah@hibank.co.id>
2026-07-22 13:32:38 +0700

    feat: add aws-ssm helpers, consolidate completions and aliases

--- awscli-profile-helpers/aws-profile-completion.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-profiles="$(grep '\[profile' ~/.aws/config | tr -d '\[\]' | awk '{print $2}')"
-
-complete -W "${profiles}" aws-profile
-complete -W "${profiles}" aws-refresh-credentials
